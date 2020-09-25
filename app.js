4b10d3869a0d367fd170e70fbc1f817ac97bbc7e
张文旗 <1071942338@qq.com>
2020-09-25 16:11:31 +0800

    04-处理不同类型请求参数

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-//1.导入http模块
+//1.导入模块
 const http = require("http");
+const queryString = require("querystring");
+
 //2、创建服务对象
 const server = http.createServer();
+
 //3、监听用户请求
 server.on("request", (req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
+
     //获取请求类型
     const method = req.method.toLowerCase();
+    //获取请求地址
+    const reqUrl = req.url;
     //根据不同请求类型，进行相应处理
     if (method === "get") {
-        res.end(`请求方法为:${method}`);
+        //通过字符串分割获取参数部分
+        const paramsStr = reqUrl.split("?")[1];
+        //通过 querystring ，解析成对象，方便使用
+        const paramsObj = queryString.parse(paramsStr);
+        res.end(`请求方法为:${method} 参数为:${JSON.stringify(paramsObj)}`);
     } else if (method === "post") {
-        res.end(`请求方法为:${method}`);
+        //请求体通过监听 'data' 和 'end' 事件,拼接数据流并且把它转化为字符串
+        let paramsBuff = [];
+        req.on("data", (chuck) => {
+            //拼接buff
+            paramsBuff.push(chuck);
+        });
+        req.on("end", () => {
+            //buff 转换成字符串
+            const paramsStr = paramsBuff.toString();
+            //通过 querystring ，解析成对象，方便使用
+            const paramsObj = queryString.parse(paramsStr);
+            res.end(`请求方法为:${method} 参数为:${JSON.stringify(paramsObj)}`);
+        });
     } else {
         res.end(`404 Not Found`);
     }
 });
+
 //4、指定监听端口
 server.listen(3000);
