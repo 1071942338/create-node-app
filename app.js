1439beab7416ce814f9ab156002d63b190554601
张文旗 <1071942338@qq.com>
2020-09-25 15:33:33 +0800

    03-处理不同类型请求

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const server = http.createServer();
 server.on("request", (req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
-    res.end("Hello Node.js");
+    //获取请求类型
+    const method = req.method.toLowerCase();
+    //根据不同请求类型，进行相应处理
+    if (method === "get") {
+        res.end(`请求方法为:${method}`);
+    } else if (method === "post") {
+        res.end(`请求方法为:${method}`);
+    } else {
+        res.end(`404 Not Found`);
+    }
 });
 //4、指定监听端口
 server.listen(3000);
